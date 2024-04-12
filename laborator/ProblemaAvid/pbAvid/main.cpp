2adff524464771514e38df1d6099206188d2124c
Lar <larisami12@yahoo.com>
2024-04-12 18:28:04 +0300

    rezolvare problema avid

--- laborator/ProblemaAvid/pbAvid/main.cpp
+++ laborator/ProblemaAvid/pbAvid/main.cpp
@@ -40,9 +40,6 @@ int main()
     for(int index = 0;index < numbersCount; index++){
         fin >> numbers[index];
     }
-    /*for(int i = 0;i < n; i++){
-        cout << v[i] << " ";
-    }*/
     int numarTriplete = 0;
     if(cerinta == 1){
         for(int index = 0; index <= numbersCount - 3; index++){
@@ -57,28 +54,25 @@ int main()
     }
     if(cerinta == 2){
         int secventaMaxima = 0;
-        int numereInSecventa = 0;
-         for(int index = 0; index <= numbersCount - 3; index++){
+        int numereInSecventa = 2;
+        for(int index = 0; index <= numbersCount - 3; index++){
              int firstCmmdc = getCmmdc(numbers[index],numbers[index + 1]);
              int cmmdc = getCmmdc(firstCmmdc, numbers[index + 2]);
              int numarDivizori = getNumarDivizori(cmmdc);
              if(numarDivizori <= numarMaximDivizori){
                 numereInSecventa++;
              }
-             if(numarDivizori > numarMaximDivizori){
-                if(numereInSecventa > secventaMaxima){
-                    secventaMaxima = numereInSecventa;
-                }
-                numereInSecventa = 0;
+             else{
+                numereInSecventa = 2;
              }
-
+             if(numereInSecventa > secventaMaxima){
+                secventaMaxima = numereInSecventa;
+             }
+        }
+        if(secventaMaxima == 2){
+            secventaMaxima = 0;
          }
-         secventaMaxima = numereInSecventa;
-         fout << secventaMaxima;
+        fout << secventaMaxima;
     }
-
-    /*2. Care este lungimea maximă a unei secvențe din șirul dat, în care cel mai mare divizor comun
-al oricărui triplet de numere situate pe poziții consecutive are cel mult 𝑝 divizori naturali?
-*/
     return 0;
 }
